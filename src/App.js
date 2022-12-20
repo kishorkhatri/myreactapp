6265b4dfeb6cf1f6acfd9b196178628240302b02
kishorkhatri <32215455+kishorkhatri@users.noreply.github.com>
2022-12-20 21:48:03 +0530

    form added button

--- src/App.js
+++ src/App.js
@@ -5,16 +5,11 @@ import Textform from './components/Textform';
 function App() {
   return (
     <>
-    <Navbar 
-    SiteName="MyReactApp" 
-    HomeText="Home" 
-    BlogText="Blog" 
-    AboutusText="About Us"
-    ContactusText="Contact Us"
-    PrivacyPolicyText="Privacy Policy"
-    DisclaimerText="Disclaimer"
-    />
-    <Textform/>
+    <Navbar SiteName="MyReactApp" HomeText="Home" BlogText="Blog" AboutusText="About Us" ContactusText="Contact Us" PrivacyPolicyText="Privacy Policy" DisclaimerText="Disclaimer"/>
+    <div className="container my-3">
+    <Textform heading="Enter your Text to analyze"/>
+      
+      </div>
     </>
   );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-export default function Textform () {
+export default function Textform (props) {
   return (
-    <div class="mb-3">
-  <label for="mybox" class="form-label">Example textarea</label>
-  <textarea class="form-control" id="mybox" rows="3"></textarea>
+    <div>
+    <h1>{props.heading}</h1>
+    <div className="mb-3">
+    <textarea className="form-control" id="mybox" rows="8"></textarea>
+</div>
+    <button className="btn btn-primary">Convert to Uppercase</button>
 </div>
   )
 }
